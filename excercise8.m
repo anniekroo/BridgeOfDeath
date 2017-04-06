3d47622e94f620952518ae5e088845f98d7fc1ae
Anne Kroo <akroo@olin.edu>
2017-04-06 02:07:40 -0400

    Adding commented code

--- excercise8.m
+++ excercise8.m
@@ -1,10 +1,10 @@
+%To initialize run the following line to set up ros's connection to matlab
 % rosinit('10.0.75.2',11311,'NodeHost','10.0.75.1')
+%-------------------------------------------------------------------------%
 syms u;
 % encode the fact that u is a real number (allows simplifications)
 assume(u,'real');
-a = .4;
-l = .4;
-% create a symbolic expression for an ellipse
+% create a symbolic expression for ellipse
 R = sym([.5*cos(u), .75*sin(u), 0]);
 
 % compute the tangent vector
@@ -14,36 +14,31 @@ That = simplify(T ./ norm(T));
 N = simplify(diff(That));
 Bhat = simplify(cross(That, N));
 %-------------------------------------------------------------------------%
-pub = rospublisher('/raw_vel');
-sub_bump = rossubscriber('/bump');
-msg = rosmessage(pub);
-d = 0.24765;                              %m
-w = Bhat(3);
-v = simplify(norm(T));                                  %theoretical R of d
-vR = w.*((v./w)+(d./2));
-vL = w.*((v./w)-(d./2));
-endTime = 20;
-sub_enc = rossubscriber('/encoders');
-%TODO figure out how to have echo not take over your program
-%TODO write velocities to matrix
-tStart = tic();
-elapsed = 0;
-pos = [];
-sampleTime = .1;
-timeStep = .1;
-while elapsed <= endTime
-    for i = 0:timeStep:endTime
-        startLoopTime = tic();
-        u = i/3;
-        instVR = (double (subs(vR)))/3
-        instVL = (double (subs(vL)))/3
-        msg.Data = [instVL, instVR];
-        send(pub, msg);
-        pause(timeStep - toc(startLoopTime))
+pub = rospublisher('/raw_vel'); %set up publisher for velocity to be written to later
+msg = rosmessage(pub);          %set up message of publisher for velocity
+d = 0.24765;                    %m
+w = Bhat(3);                    %equating Bhat to angular velocity
+v = simplify(norm(T));          %theoretical R of d
+
+vR = w.*((v./w)+(d./2));        % Equations for the wheels of the neto robot 
+vL = w.*((v./w)-(d./2));        % given angular velocity and linear velocity
+endTime = 17;                   % stops robot after set amount of time
+timeStep = .1;                  % determines how often we change our velocities
+
+tStart = tic();                 % starts timer counting how long the program has been running
+while elapsed <= endTime        % checks that the program has been running for less than endTime
+    for i = 0:timeStep:endTime  % runs through each time step and writes to neto
+        startLoopTime = tic();  % establishes start time for loop to check how long it takes to run loop
+        u = i/4.5;              % creates a u value to be substituted into symbolic function
+        instVR = (double (subs(vR)))/4.5;   % creates instantaneous velocity for right wheel
+        instVL = (double (subs(vL)))/4.5;   % creates instantaneous velocity for right wheel
+        msg.Data = [instVL, instVR];        % writes data to data object in message
+        send(pub, msg);                     % sends message to neto
+        pause(timeStep - toc(startLoopTime))% delays re-running the loop for timeStep seconds from the start of the loop
     end
-    elapsed = toc(tStart);
+    elapsed = toc(tStart);                  %recreates elapsed to check whether or not the while loop should end
 end
-msg.Data = [0,0];
-send(pub, msg);
-%-------------------------------------------------------------------------%
+
+msg.Data = [0,0];               % writes 0 velocities to data object of message
+send(pub, msg);                 % sends 0 velocities to neto
 %rosshutdown
